9a042297293b63d5792799ec741afa01cf4b18c0
Nicolas Llancaqueo <n.llancaqueo01@ufromail.cl>
2024-06-05 14:43:26 -0400

    cargar repositorio

---
+++ src/test/java/com/taller/tallerspringboot/TallerspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.tallerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
